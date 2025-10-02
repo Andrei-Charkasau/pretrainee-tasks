07d59b2a0f26571e586238b24414d64fe302312d
Andrei-Charkasau <frostmornfire@gmail.com>
2025-10-02 15:11:16 +0300

    Task #1: Calculator
    Uploading first task.
    Calculator.

---
+++ Task_1_Console_Calculator/Task_1_Console_Calculator/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Console_Calculator
+{
+    internal enum MathOperations
+    {
+        Unspecified = 0, 
+        Sum,
+        Divide,
+        Multiply,
+        Minus
+    }
+}
